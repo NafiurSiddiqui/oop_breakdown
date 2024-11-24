49467c1ca98349afe0540a359f6e2ea6d95eec63
NafiurSiddiqui <saejim91@gmail.com>
2024-11-25 04:31:17 +0600

    bug fixed: typo

--- Encapsulation.js
+++ Encapsulation.js
@@ -7,12 +7,11 @@ class User {
 	constructor(name, email, bankBalance, password) {
 		this.name = name;
 		this.email = email;
-		this.bankBalance = bankBalance;
+		this.#bankBalance = bankBalance;
 		this.#password = password;
 	}
 }
 
 const user = new User('Jenny', 'jen@xyz.com', 10000, 'secret');
 
-console.log(user.bankBalance); // DOES NOT THROW ANY ERROR.
 console.log(user.#bankBalance); //Throws runtime error.

06372d90580574fcf8818f886239ce5e05586502
NafiurSiddiqui <saejim91@gmail.com>
2024-12-11 22:53:28 +0530

    concept (LSP): concept finished, create some challenges for the course or book

--- 2.SOLID/L-LSP.php
+++ 2.SOLID/L-LSP.php
@@ -85,8 +85,19 @@ function moveVehicle(Vehicle $vehicle): void
     $vehicle->move();
 }
 
+function testAllVehicleMoves(array $vehicles)
+{
+    try {
+        foreach ($vehicles as $vehicle) {
+            moveVehicle($vehicle);
+        }
+    } catch (\Exception $e) {
+        echo $e->getMessage() . PHP_EOL;
+    }
+}
 
-//! Lets violate LSP in the first place
+
+//! Lets violate LSP in the first place - Violdate code starts
 //If your IDE shouts, comment out the AmphibousVehicle class at the bottom, which is used for SOLUTION.
 
 // class AmphibousVehicle extends Vehicle
@@ -99,32 +110,24 @@ function moveVehicle(Vehicle $vehicle): void
 // }
 
 //first think about it, why does it actually violates the LSP and then compare with the explanation.
-$ampy = new AmphibousVehicle('Xavier', 'warpsteer', 6);
+// $amph1 = new AmphibousVehicle('Xavier', 'warpsteer', 6);
 
 //add ampy to the $vehicles array
-$vehicles[] = $ampy;
+// $vehicles[] = $amph1;
 
+//! Violated code ends
 
-//SINCE we have deliberately throwing an error from Amphibious we need to catch it or it will break.
-
-// try {
-//     foreach ($vehicles as $vehicle) {
-//         moveVehicle($vehicle);
-//     }
-// } catch (\Exception $e) {
-//     echo $e->getMessage() . PHP_EOL;
-// }
 
 //---------- ISSUE: Explained ----------
 
-//The issue is with the amphibousVehicle. According to the LSP, you should be able to swap the parent class with the child class, and the program should still work as expected. 
-
-//SO, in our scenario, we wrote a program that moves all the vehicle. In the program, move vehicle, according to the LSP we should be able to substitute the `Vehicle` parent arugment with any of the subclass of type Vehicle without breaking the code. Each of the subclass adhered to the LSP and resepcted parents behavior and ran absolutely fine without breaking the program EXCEPT for Amphibous. 
-//We are thrwoing an error, or it could be any other code that would alter and contradict the nature of the parent class.But here is the thing, if you notice closely, there is another thing the way I wrote the Amphibous, violates the LSP.
-//Hence, we are violating the LSP and introducing a bug in our program that would take quite a bit of time or days in a large application.
-
-//So, esentially there are two bugs here. Take your time and think about it for a moment. Other than the throwing exception, what is potentially breaking the program from the amph->move() method? Don't take more than 30 mins (max)
-//Don't worry if you did not find it. Let's break it down.
+/**
+ - The issue is with the amphibousVehicle. According to the LSP, you should be able to swap the parent class with the child class, and the program should still work as expected. 
+ - SO, in our scenario, we wrote a program that moves all the vehicle. In the program, move vehicle, according to the LSP we should be able to substitute the `Vehicle` parent arugment with any of the subclass of type Vehicle without breaking the code. Each of the subclass adhered to the LSP and resepcted parents behavior and ran absolutely fine without breaking the program EXCEPT for Amphibous.
+ - We are thrwoing an error, or it could be any other code that would alter and contradict the nature of the parent class.But here is the thing, if you notice closely, there is another thing the way I wrote the Amphibous, violates the LSP.
+ - Hence, we are violating the LSP and introducing a bug in our program that would take quite a bit of time or days in a large application.
+ - So, esentially there are two bugs here. Take your time and think about it for a moment. Other than the throwing exception, what is potentially breaking the program from the amph->move() method? Don't take more than 30 mins (max)
+ - Don't worry if you did not find it. Let's break it down.
+ */
 
 
 //? ----------- What is the issue, how do we fix it?
@@ -136,7 +139,9 @@ $vehicles[] = $ampy;
  */
 
 
-//?------- FIX
+//?------- FIX: LSP code start
+
+//COMMENT OUT THE VIOLATED CODE FIRST and proceed down here
 
 //very first thing we will take out the error
 //second, we will introduce an additional method of switching the Mode before move method is called as a mandatory.
@@ -154,69 +159,61 @@ class AmphibousVehicle extends Vehicle
     private int $modeCounter = 0;
     // private string $mode = $modes[0]; //Throws error because you can not init a class property outside of the regular types like bool, int, string. Array won't work.
 
-    public string $mode = 'land';
+    private string $mode = 'land';
 
-    public function __construct()
-    {
-        $this->mode = $this->modes[$this->modeCounter];
-    }
 
     public function move(): void
     {
-        echo $this->getManufacturerModel() . " is moving in the outer space,air, over the water, on the land" . PHP_EOL;
+        echo $this->getManufacturerModel() . " is moving " . $this->moveMessageFormatter() . PHP_EOL;
         //took out the error we introduced
     }
-
-    // public function switchMode(): void
-    // {
-    //     //each time we call switch
-    //     //it changes the mode within the given mode
-    //     //get the modes length
-    //     // print_r(end($this->modes));
-    //     // print_r(prev($this->modes));
-
-    //     if ($this->mode === end($this->modes)) {
-    //         var_dump('first block');
-    //         // $this->modeCounter = 0;
-    //     } else {
-    //         // var_dump('second block');
-    //         // $this->setMode();
-    //         echo "$this->mode";
-    //         $this->mode = next($this->modes);
-    //         echo "$this->mode";
-    //     }
-    //     //increase the $modes[counter] as long as it is within the count.
-    //     //set the increased mode to the $this->mode.
-    //     //if current mode is equal to the count() of the mode, set the mode to zero index again.
-
-    // }
 
     public function switchMode(): void
     {
         $this->modeCounter = ($this->modeCounter + 1) % count($this->modes);
         $this->mode = $this->modes[$this->modeCounter];
+        echo  "Amphibous switched to $this->mode";
     }
-
+    //GETTER (Encapsulation)
     public function getMode(): void
     {
 
         echo $this->mode;
+    }
+    //DRY code
+    public function moveMessageFormatter(): string
+    {
+        return match ($this->mode) {
+            'land' => 'on the land',
+            'water' => 'over the water',
+            'air' => 'in the air',
+            'outer space' => 'in the outer space',
+            'default' => 'on the land'
+        };
     }
 }
 
 
 $amph2 = new AmphibousVehicle('Xavier', 'warpsteer2', 6);
 
-$amph2->getMode();
+// $amph2->getMode();
 echo "\n";
+// $amph2->switchMode();
+// $amph2->move();
+
+//add $amph2 to the $vehicles array
+$vehicles[] = $amph2;
+//Now if we call our program to move all vehicles and remove apmh1 from the $vehicles array our program should run adhering the LSP
+
+testAllVehicleMoves($vehicles);
+echo "Amphibous Switching Mode \n";
+//Lets switch mode for amph2
 $amph2->switchMode();
-$amph2->getMode();
-// echo "\n";
-// $amph2->switchMode();
-// $amph2->getMode();
-// echo "\n";
-// $amph2->switchMode();
-// $amph2->getMode();
-// echo "\n";
-// $amph2->switchMode();
-// $amph2->getMode();
+echo "\n";
+testAllVehicleMoves($vehicles);
+
+
+//conclusion - now read the L-LSP.readme, and you will understand the theoritical aspect better.
+
+//TODO
+//Create some challenges/quiz to test the students if they can detect when a LSP is violated and not.
